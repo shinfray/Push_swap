bbf8d1e9d939bb9a1e93b552164779eda97824c0
shinfray <simon@hinfray.eu>
2023-06-16 16:40:55 +0200

    update dllist functions

--- src/dllist.c
+++ src/dllist.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 15:52:37 by shinfray          #+#    #+#             */
-/*   Updated: 2023/06/16 10:19:39 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/06/16 16:38:05 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,36 +111,12 @@ t_dllist_node	*ft_dllist_insert_after(t_dllist_node *node, int value)
 
 t_dllist_node	*ft_dllist_prepend(t_dllist *dllist, int value)
 {
-	t_dllist_node	*new_node;
-	t_dllist_node	*next_node;
-
-	next_node = ft_dllist_first(dllist);
-	new_node = ft_calloc(1, sizeof(*new_node));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->val = value;
-	new_node->next = next_node;
-	new_node->prev = dllist->sentinel_node;
-	dllist->sentinel_node->next = new_node;
-	next_node->prev = new_node;
-	return (new_node);
+	return (ft_dllist_insert_after(dllist->sentinel_node, value));
 }
 
 t_dllist_node	*ft_dllist_append(t_dllist *dllist, int value)
 {
-	t_dllist_node	*new_node;
-	t_dllist_node	*prev_node;
-
-	prev_node = ft_dllist_last(dllist);
-	new_node = ft_calloc(1, sizeof(*new_node));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->val = value;
-	new_node->next = dllist->sentinel_node;
-	new_node->prev = prev_node;
-	dllist->sentinel_node->prev = new_node;
-	prev_node->next = new_node;
-	return (new_node);
+	return (ft_dllist_insert_before(dllist->sentinel_node, value));
 }
 
 t_dllist_node	*ft_dllist_next(t_dllist_node *node)
