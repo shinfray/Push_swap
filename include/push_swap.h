0d322bd8d4d7d32afa1b26bdebf1eadbea569ba7
shinfray <simon@hinfray.eu>
2023-06-17 15:10:32 +0200

    update norm

--- include/push_swap.h
+++ include/push_swap.h
@@ -5,52 +5,49 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/15 11:06:36 by shinfray          #+#    #+#             */
-/*   Updated: 2023/06/17 14:25:50 by shinfray         ###   ########.fr       */
+/*   Created: 2023/06/17 14:55:25 by shinfray          #+#    #+#             */
+/*   Updated: 2023/06/17 15:04:51 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP
-# define PUSH_SWAP
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 # include <limits.h>
 # include <stdbool.h>
 # include <stdlib.h>
 # include "libft.h"
 
-/*  to remove   */
-#include <stdio.h>
-
 typedef struct dllist_node
 {
-    struct dllist_node  *next;
-    struct dllist_node  *prev;
-    int                 val;
-}               t_dllist_node;
+	struct dllist_node	*next;
+	struct dllist_node	*prev;
+	int					val;
+}				t_dllist_node;
 
 typedef struct dllist
 {
-    t_dllist_node   *sentinel_node;
+	t_dllist_node	*sentinel_node;
 	size_t			total_nodes;
-}               t_dllist;
+}				t_dllist;
 
 /*
  *	PARSE FUNCTIONS
 */
-t_dllist	*ft_retrieve_args(t_dllist *dllist, int argc, char **argv);
+t_dllist		*ft_retrieve_args(t_dllist *dllist, int argc, char **argv);
 
 /*
  *	DLLIST FUNCTIONS
 */
 /*  DLLIST_ACCESSOR    */
-t_dllist_node 	*ft_dllist_next(t_dllist_node *node);
+t_dllist_node	*ft_dllist_next(t_dllist_node *node);
 t_dllist_node	*ft_dllist_prev(t_dllist_node *node);
 t_dllist_node	*ft_dllist_first(t_dllist *dllist);
 t_dllist_node	*ft_dllist_last(t_dllist *dllist);
 /*	DLLIST_CREATION_DESTRUCTION	*/
-t_dllist	*ft_new_dllist(void);
-void		ft_free_dllist(t_dllist *dllist);
-bool		ft_dllist_is_empty(t_dllist *dllist);
+t_dllist		*ft_new_dllist(void);
+void			ft_free_dllist(t_dllist *dllist);
+bool			ft_dllist_is_empty(t_dllist *dllist);
 /*	DLLIST_INSERTION	*/
 t_dllist_node	*ft_dllist_append(t_dllist *dllist, int value);
 
@@ -58,19 +55,19 @@ t_dllist_node	*ft_dllist_append(t_dllist *dllist, int value);
  *	MOVES FUNCTIONS
 */
 /*	SWAP	*/
-void	ft_sa(t_dllist *stack_a, t_dllist *stack_b);
-void	ft_sb(t_dllist *stack_a, t_dllist *stack_b);
-void	ft_ss(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_sa(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_sb(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_ss(t_dllist *stack_a, t_dllist *stack_b);
 /*	PUSH	*/
-void	ft_pa(t_dllist *stack_a, t_dllist *stack_b);
-void	ft_pb(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_pa(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_pb(t_dllist *stack_a, t_dllist *stack_b);
 /*	ROTATE	*/
-void	ft_ra(t_dllist *stack_a, t_dllist *stack_b);
-void	ft_rb(t_dllist *stack_a, t_dllist *stack_b);
-void	ft_rr(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_ra(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_rb(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_rr(t_dllist *stack_a, t_dllist *stack_b);
 /*	REVERSE ROTATE	*/
-void	ft_rra(t_dllist *stack_a, t_dllist *stack_b);
-void	ft_rrb(t_dllist *stack_a, t_dllist *stack_b);
-void	ft_rrr(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_rra(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_rrb(t_dllist *stack_a, t_dllist *stack_b);
+void			ft_rrr(t_dllist *stack_a, t_dllist *stack_b);
 
 #endif
