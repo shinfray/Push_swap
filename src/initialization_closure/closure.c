9e557c02b6b18f90dea1a66e6d4e136b3611f620
shinfray <simon@hinfray.eu>
2023-06-20 09:42:06 +0200

    update

--- src/initialization_closure/closure.c
+++ src/initialization_closure/closure.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 15:36:29 by shinfray          #+#    #+#             */
-/*   Updated: 2023/06/20 09:37:35 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/06/20 09:41:40 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,3 @@ void	ft_free_args_list(char **args_list)
 		free(*args_list++);
 	free(save);
 }
-
-// void	ft_free_push_swap(t_dllist *stack_a, t_dllist *stack_b, char **args_list, char **argv)
-// {
-// 		ft_free_stacks(stack_a , stack_b);
-// 		if (args_list != NULL && args_list != argv + 1)
-// 			ft_free_args_list(args_list);
-// }
