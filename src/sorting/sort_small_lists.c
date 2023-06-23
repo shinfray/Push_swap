8ae02fa6cdc8bd8370cb312c195581155e009e5a
shinfray <simon@hinfray.eu>
2023-06-23 10:24:04 +0200

    update

--- src/sorting/sort_small_lists.c
+++ src/sorting/sort_small_lists.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 11:50:56 by shinfray          #+#    #+#             */
-/*   Updated: 2023/06/22 23:25:05 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/06/23 10:22:19 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,41 @@ void	ft_sort_list_up_to_3(t_dllist *stack_a)
 	}
 }
 
+void	ft_sort_list_up_to_5(t_dllist *stack_a, t_dllist *stack_b)
+{
+	while (stack_a->total_nodes > 3)
+		ft_pb(stack_a, stack_b);
+	if (ft_stack_is_sorted(stack_a) == false)
+		ft_sort_list_up_to_3(stack_a);
+	while (ft_dllist_is_empty(stack_b) == false)
+	{
+		if (ft_dllist_first(stack_b)->val < ft_dllist_first(stack_a)->val)
+			ft_pa(stack_a, stack_b);
+		else if (ft_dllist_first(stack_b)->val > ft_dllist_last(stack_a)->val)
+		{
+			ft_pa(stack_a, stack_b);
+			ft_ra(stack_a, stack_b);
+		}
+		else
+		{
+			while (ft_dllist_first(stack_b)->val \
+					> ft_dllist_first(stack_a)->val)
+				ft_ra(stack_a, stack_b);
+			ft_pa(stack_a, stack_b);
+			while (ft_stack_is_sorted(stack_a) == false)
+				ft_rra(stack_a, stack_b);
+		}
+	}
+}
+
+
 // void	ft_sort_list_up_to_5(t_dllist *stack_a, t_dllist *stack_b)
 // {
 // 	while (stack_a->total_nodes > 3)
 // 		ft_pb(stack_a, stack_b);
 // 	if (ft_stack_is_sorted(stack_a) == false)
 // 		ft_sort_list_up_to_3(stack_a);
-// 	while (ft_dllist_is_empty(stack_b) == false)
+// 	if (ft_dllist_is_empty(stack_b) == false)
 // 	{
 // 		if (ft_dllist_first(stack_b)->val < ft_dllist_first(stack_a)->val)
 // 			ft_pa(stack_a, stack_b);
@@ -61,6 +89,32 @@ void	ft_sort_list_up_to_3(t_dllist *stack_a)
 // 		}
 // 		else
 // 		{
+// 			if (ft_dllist_first(stack_b)->val < ft_dllist_next(ft_dllist_first(stack_a))->val)
+// 			{
+// 				ft_pa(stack_a, stack_b);
+// 				ft_sa(stack_a, stack_b);
+// 			}
+// 			else
+// 			{
+// 				ft_rra(stack_a, stack_b);
+// 				ft_pa(stack_a, stack_b);
+// 				ft_ra(stack_a, stack_b);
+// 				ft_ra(stack_a, stack_b);
+// 			}
+// 		}
+// 	}
+// 	if (ft_dllist_is_empty(stack_b) == false)
+// 	{
+// 		if (ft_dllist_first(stack_b)->val < ft_dllist_first(stack_a)->val)
+// 			ft_pa(stack_a, stack_b);
+// 		else if (ft_dllist_first(stack_b)->val > ft_dllist_last(stack_a)->val)
+// 		{
+// 			ft_pa(stack_a, stack_b);
+// 			ft_ra(stack_a, stack_b);
+// 		}
+// 		else
+// 		{
+// 			// optimize this code
 // 			while (ft_dllist_first(stack_b)->val \
 // 					> ft_dllist_first(stack_a)->val)
 // 				ft_ra(stack_a, stack_b);
@@ -68,56 +122,6 @@ void	ft_sort_list_up_to_3(t_dllist *stack_a)
 // 			while (ft_stack_is_sorted(stack_a) == false)
 // 				ft_rra(stack_a, stack_b);
 // 		}
+
 // 	}
 // }
-
-
-void	ft_sort_list_up_to_5(t_dllist *stack_a, t_dllist *stack_b)
-{
-	while (stack_a->total_nodes > 3)
-		ft_pb(stack_a, stack_b);
-	if (ft_stack_is_sorted(stack_a) == false)
-		ft_sort_list_up_to_3(stack_a);
-	if (ft_dllist_is_empty(stack_b) == false)
-	{
-		if (ft_dllist_first(stack_b)->val < ft_dllist_first(stack_a)->val)
-			ft_pa(stack_a, stack_b);
-		else if (ft_dllist_first(stack_b)->val > ft_dllist_last(stack_a)->val)
-		{
-			ft_pa(stack_a, stack_b);
-			ft_ra(stack_a, stack_b);
-		}
-		else
-		{
-			if (ft_dllist_first(stack_b)->val < ft_dllist_next(ft_dllist_first(stack_a))->val)
-			{
-				ft_pa(stack_a, stack_b);
-				ft_sa(stack_a, stack_b);
-			}
-			else
-			{
-				ft_rra(stack_a, stack_b);
-				ft_pa(stack_a, stack_b);
-				ft_ra(stack_a, stack_b);
-				ft_ra(stack_a, stack_b);
-			}
-		}
-	}
-	if (ft_dllist_is_empty(stack_b) == false)
-	{
-		if (ft_dllist_first(stack_b)->val < ft_dllist_first(stack_a)->val)
-			ft_pa(stack_a, stack_b);
-		else if (ft_dllist_first(stack_b)->val > ft_dllist_last(stack_a)->val)
-		{
-			ft_pa(stack_a, stack_b);
-			ft_ra(stack_a, stack_b);
-		}
-		else
-		{
-			//
-			//
-			//		
-		}
-
-	}
-}
